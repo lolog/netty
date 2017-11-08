08a073efe4552d612499b99f86af6b21a1377efa
lolog <jonguo@yeah.net>
2017-11-09 00:34:09 +0800

    protobuf test

--- netty-plugins/src/main/java/net/netty/plugins/test/ProtobufTest.java
+++ netty-plugins/src/main/java/net/netty/plugins/test/ProtobufTest.java
@@ -1,4 +1,33 @@
 package net.netty.plugins.test;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import net.netty.plugins.pojo.User;
+
 public class ProtobufTest {
+	byte[] encode(User.UserInfo userInfo) {
+		return userInfo.toByteArray();
+	}
+	User.UserInfo decode (byte[] body) throws InvalidProtocolBufferException {
+		return User.UserInfo.parseFrom(body);
+	}
+	User.UserInfo createUserinfo () {
+		User.UserInfo.Builder builder = User.UserInfo.newBuilder();
+		
+		builder.setName("name");
+		builder.setAge(26);
+		builder.setInfo("Uway");
+		
+		return builder.build();
+	}
+	
+	public static void main(String[] args) throws InvalidProtocolBufferException {
+		ProtobufTest protobufTest = new ProtobufTest();
+		
+		User.UserInfo userInfo = protobufTest.createUserinfo();
+		System.out.println("useroinfo = " + userInfo.toString());
+		System.out.println("[encode] \n" + new String(protobufTest.encode(userInfo)));
+		System.out.println("[decode] \n" + protobufTest.decode(protobufTest.encode(userInfo)));
+		
+	}
 }
--- netty-server/src/main/java/net/netty/server/thread/MessagePackServer.java
+++ netty-server/src/main/java/net/netty/server/thread/MessagePackServer.java
@@ -1,5 +1,7 @@
 package net.netty.server.thread;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
@@ -10,9 +12,6 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.netty.handler.codec.LengthFieldPrepender;
-
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import net.netty.plugins.decoder.message.pack.MessagePackDecoder;
 import net.netty.plugins.encoder.message.pack.MessagePackEncoder;
 import net.netty.server.handler.MessagePackHandler;
